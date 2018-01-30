93cdabb5740377b298642ca8a805efb1e7c14552
michael-small <33669563+michael-small@users.noreply.github.com>
2018-01-29 22:20:14 -0600

    Update inversepyramid.py

--- inversepyramid.py
+++ inversepyramid.py
@@ -1,14 +1,7 @@
-def pyramid(char, odd_number):
-  space = " " 
-  space_numb = 0
-  while odd_number > -1:
-    print space_numb*space, char*odd_number
-    if char == "%":
-      space_numb += 3
-      odd_number -= 2
-    elif char == "=":
-      space_numb += 2
-      odd_number -= 2
-    else:
-      space_numb += 1
-      odd_number -= 2
+def invertedPyramid(char, odd_number):
+    space = " "
+    space_numb = 0
+    while odd_number > -1:
+        print space_numb*space, char*odd_number
+        space_numb += 1
+        odd_number -= 2
